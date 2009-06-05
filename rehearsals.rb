f5629bdd0fe947d9a536350337a3ad900be1d3cc
Phil Oye <philoye@philoye.com>
2009-06-05 16:04:25 +1000

    Slight reorg

--- rehearsals.rb
+++ rehearsals.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'haml'
+require "#{File.join(File.dirname(__FILE__),'lib','git-tags.rb')}"
 
 module Rehearsals
   class Application < Sinatra::Base
@@ -8,8 +9,6 @@ module Rehearsals
     set :public, File.join(File.expand_path(File.dirname(__FILE__)),'public')
     set :views, File.join(File.expand_path(File.dirname(__FILE__)),'views')
 
-    require "#{File.join(File.dirname(__FILE__),'lib','git-tags.rb')}"
-  
     get '/' do
       @tags = GitTags.export_tags
       haml :index 
