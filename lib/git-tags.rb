7e9240e4b33391dd1532d9572e095b09ab24d249
Phil Oye <philoye@philoye.com>
2009-06-10 16:54:49 +1000

    Using class variables instead of passing them around...

--- lib/git-tags.rb
+++ lib/git-tags.rb
@@ -3,17 +3,18 @@ require 'fileutils'
 
 module GitTags
 
+  @path_to_repo = File.join(File.expand_path('../..',File.dirname(__FILE__)))
+  @tags_dir = File.join(File.expand_path(File.dirname(__FILE__)),'tags')
+
   def self.export_tags
-    path_to_repo = File.join(File.expand_path('../..',File.dirname(__FILE__)))
-    tags_dir = File.join(File.expand_path(File.dirname(__FILE__)),'tags')
-    FileUtils.mkdir_p tags_dir
-    update_master(tags_dir,path_to_repo)
-    update_tags(tags_dir,path_to_repo)
+    FileUtils.mkdir_p @tags_dir
+    update_master
+    update_tags
     return tags
   end
   def self.tags
     tags = []
-    text = `cd .. && git tag -n`
+    text = `cd #{@path_to_repo} && git tag -n`
     lines = text.split("\n")
     lines.each do |line|
       h = {}
@@ -24,20 +25,20 @@ module GitTags
     end
     return tags
   end
-  def self.update_master(tags_dir,path_to_repo)
-    unless File.directory? "#{tags_dir}/master"
-      `git clone #{path_to_repo} #{tags_dir}/master`
+  def self.update_master
+    unless File.directory? "#{@tags_dir}/master"
+      `git clone #{@path_to_repo} #{@tags_dir}/master`
     end
-    Dir.chdir("#{tags_dir}/master") do
+    Dir.chdir("#{@tags_dir}/master") do
       `git pull origin master --quiet`
       `git reset --hard master`
     end
   end
-  def self.update_tags(tags_dir,path_to_repo)
+  def self.update_tags
     tags.each do |tag|
-      unless File.directory? "#{tags_dir}/#{tag['name']}" 
-        `git clone #{path_to_repo} #{tags_dir}/#{tag['name']}`
-        Dir.chdir("#{tags_dir}/#{tag['name']}") do
+      unless File.directory? "#{@tags_dir}/#{tag['name']}" 
+        `git clone #{@path_to_repo} #{@tags_dir}/#{tag['name']}`
+        Dir.chdir("#{@tags_dir}/#{tag['name']}") do
           `git reset --hard #{tag['name']}`
         end
       end
